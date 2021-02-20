96460d5b38dcd0d12452a02104d718b18fb40df1
tech-bandits <79304960+tech-bandits@users.noreply.github.com>
2021-02-20 16:41:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import pandas as pd
 import datetime
-#import smtplib
-#import os
-#os.chdir(r"C:\Users\E20CS\Birthday Bot")
+import smtplib
+import os
+os.chdir(r"C:\Users\E20CS\Birthday Bot")
 # os.mkdir("testing") 
 
 # Enter your authentication details
-#GMAIL_ID = ""
-#GMAIL_PSWD = ""
+GMAIL_ID = ""
+GMAIL_PSWD = ""
 
 
 def sendEmail(to, sub, msg):
-    #print(f"Email to {to} sent with subject: {sub} and message {msg}" )
-    #s = smtplib.SMTP('E20CSE113@bennett.edu.in', 587)
-    #s.starttls()
-    #s.login(GMAIL_ID, GMAIL_PSWD)
-    #s.sendmail(GMAIL_ID, to, f"Subject: {sub}\n\n{msg}")
-    #s.quit()'''
-    pass
+    print(f"Email to {to} sent with subject: {sub} and message {msg}" )
+    s = smtplib.SMTP('E20CSE113@bennett.edu.in', 587)
+    s.starttls()
+    s.login(GMAIL_ID, GMAIL_PSWD)
+    s.sendmail(GMAIL_ID, to, f"Subject: {sub}\n\n{msg}")
+    s.quit()'''
+    
     
 
 if __name__ == "__main__":
@@ -28,22 +28,22 @@ if __name__ == "__main__":
     df = pd.read_excel("Birthday_Dates.xlsx")
     # print(df)
     today = datetime.datetime.now().strftime("%d-%m")
-    #yearNow = datetime.datetime.now().strftime("%Y")
+    yearNow = datetime.datetime.now().strftime("%Y")
     # print(type(today))
-    #writeInd = []
+    writeInd = []
     for index, item in df.iterrows():
-        print(index, item['Birthday'])
-       # bday = item['Birthday'].strftime("%d-%m")
+        #print(index, item['Birthday'])
+        bday = item['Birthday'].strftime("%d-%m")
         # print(bday) 
-        #if(today == bday) and yearNow not in str(item['Year']):
-         #   sendEmail(item['Email'], "Happy Birthday", item['Dialogue']) 
-          #  writeInd.append(index)
+        if(today == bday) and yearNow not in str(item['Year']):
+            sendEmail(item['Email'], "Happy Birthday", item['Dialogue']) 
+            writeInd.append(index)
 
     # print(writeInd)
-    #for i in writeInd:
-    #    yr = df.loc[i, 'Year']
-     #   df.loc[i, 'Year'] = str(yr) + ', ' + str(yearNow)
+    for i in writeInd:
+        yr = df.loc[i, 'Year']
+        df.loc[i, 'Year'] = str(yr) + ', ' + str(yearNow)
         # print(df.loc[i, 'Year'])
 
     # print(df) 
-   # df.to_excel('Birthday_Dates.xlsx', index=False)
+    df.to_excel('Birthday_Dates.xlsx', index=False)
